d55ddf76e077a83d693cb6731a7051432a47dd72
ldl <ldl131710@163.comm>
2019-07-08 04:30:56 +0800

    项目搭建

---
+++ src/main/java/aaa/ldldemo/LdldemoApplication.java
@@ -0,0 +1,13 @@
+package aaa.ldldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdldemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdldemoApplication.class, args);
+    }
+
+}
